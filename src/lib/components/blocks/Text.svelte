3705e871e28d699949252efb1f4249cafde51c61
Dirk Porsche <dirkporsche78@googlemail.com>
2024-01-15 09:19:47 +0100

    fix: text block

--- src/lib/components/blocks/Text.svelte
+++ src/lib/components/blocks/Text.svelte
@@ -1,93 +1,27 @@
 <script lang="ts">
   import { currentLang } from '$lib/stores/lang';
   import { dictEntry } from '$lib/conf/translations';
-  import { onDestroy, onMount } from 'svelte';
-  import type { Unsubscriber } from 'svelte/motion';
   export let ref:string
   export let minHeight='100%'
-  export let minFontSize='0.9rem'
-  export let maxFontSize='4.5rem'
-  export let hud=false
+  export let textFontSize='1rem'
+  export let headerFontSize='1.5rem'
 
-  let element:HTMLDivElement;
-  let hiddenElement: HTMLDivElement;
-
-  let hiddenArea:number;// = hiddenElement?.clientWidth * hiddenElement?.clientHeight;
-  let containerArea:number;// = element?.clientWidth * element?.clientHeight;
-  let ratio:number;// = containerArea / hiddenArea
-  let fontSize:number;// = (Math.round( Math.max(0, (Math.log( ratio ))) * 100 ) / 100) + 0.8;
-  
-  const calcFontSize = ():string => {
-    hiddenArea = hiddenElement?.clientWidth * hiddenElement?.clientHeight
-    containerArea = element?.clientWidth * element?.clientHeight;
-    ratio = containerArea / hiddenArea
-    let fontSize = (Math.round( Math.max(0, (Math.log( ratio ))) * 100 ) / 100) + 0.8;
-    if(fontSize == Infinity) {
-      //console.log('Calculated fontSize == Infinity')
-      fontSize = 2;
-    }
-    if(!fontSize) {
-      //console.log('Calculated fontSize falsy');
-      fontSize = 0.9;
-    }
-    return `clamp(${minFontSize ? minFontSize : '0.9rem'}, ${fontSize}rem, ${maxFontSize ? maxFontSize : '4.5rem'})`;
-  }
-
-  const resized = (n:UIEvent):any => {
-    //element.attributeStyleMap.set('font-size', minFontSize ? minFontSize : '0.9rem')
-    element.style.setProperty('font-size', minFontSize ? minFontSize : '0.9rem')
-    const newFontSize = calcFontSize();
-    element.style.setProperty('font-size', newFontSize)
-    //element.attributeStyleMap.set('font-size', newFontSize)
-  }
-
-  let unsub:Unsubscriber
-  onMount( () => {
-
-    unsub = currentLang.subscribe( (lang) => {
-      let content = dictEntry(lang, ref);
-      hiddenElement.innerHTML = content;
-      
-      //element.attributeStyleMap.set('font-size', minFontSize ? minFontSize : '0.9rem')
-      element.style.setProperty('font-size', minFontSize ? minFontSize : '0.9rem')
-
-      element.innerHTML = content;
-
-      const newFontSize = calcFontSize();
-      //element.attributeStyleMap.set('font-size', newFontSize)
-      element.style.setProperty('font-size', newFontSize)
-    });
-  })
-
-  onDestroy( () => {
-    if(unsub) unsub()
-  })
 </script>
 
-<svelte:window on:resize={resized}/>
-
 <div class="container">
-  {#if hud}
-    <div class="hud">
-    containerArea: { containerArea }<br>
-    hiddenArea: { hiddenArea }<br>
-    r: { ratio }<br>
-    log(r): { Math.log(ratio) }<br>
-    fontSize: { fontSize }
-    </div>
-  {/if}
 
-  <div 
-  class="hidden"
-  bind:this={hiddenElement}
-  />
-
-  <div 
-      style="min-height: {minHeight};"
+  <div
+      style="
+        min-height: {minHeight};
+        --font-size-text-text-editor: {textFontSize};
+        --font-size-header-text-editor: {headerFontSize};
+        "
       class="content" 
-      id="content"
-      bind:this={element}
-    />
+      id="content">
+
+    {@html dictEntry($currentLang, ref)}  
+    
+  </div>
 </div>
 
 <style>
@@ -100,23 +34,12 @@
     margin-right: 0.2rem;
   }
 
-  .hidden {
-    visibility: hidden;
-    position: absolute;
-    
-    margin-left: 0.2rem;
-    margin-right: 0.2rem;
+  .content :global(*) {
+    font-size: var(--font-size-text-text-editor);
   }
 
-  .hud {
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    padding: 1rem;
-    font-size: 0.7rem;
-    background-color:rgba(137, 43, 226, 0.8);
-    color: white;
-    border-radius: 0.5rem;
+  .content :global(h3) {
+    font-size: var(--font-size-header-text-editor);
   }
 
   .container {
--- src/lib/types/blocks.ts
+++ src/lib/types/blocks.ts
@@ -26,8 +26,8 @@ export interface TextBlock {
   content: {
     ref: string
     minHeight?: string
-    minFontSize?: string
-    maxFontSize?: string
+    textFontSize?: string
+    headerFontSize?: string
   }
 }
 
